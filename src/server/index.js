92452cf0a764f4c7ae4a43e31639a903120630c4
belaymit <2belamit@gmail.com>
2023-03-01 18:32:14 +0300

    specified path for our static files

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,13 @@
-import express from 'express';
-const app = express()
+import express from "express";
+import path from 'path';
+const root = path.join(__dirname, '../../');
+const app = express();
 
-app.get('*', (req, res) => res.send('Hello World!'));
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
 
-app.listen(8000, () => console.log('Listening on port 8000!'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(root, '/dist/client/index.html'));
+ });
+
+app.listen(8000, () => console.log("Listening on port 8000!"));
